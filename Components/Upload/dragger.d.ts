f03bb43c3e752aa8685189d78ee16ae617f55d32
LeeHS0805 <LeeHS0805@users.noreply.github.com>
2022-08-21 14:19:58 +0000

    Update github-action.yml

--- Components/Upload/dragger.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export interface DraggerProps {
-    onFile: (files: FileList) => void;
-    children?: React.ReactNode;
-}
-export declare const Dragger: React.FC<DraggerProps>;
-export default Dragger;
