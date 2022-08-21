f33f226bbd053f1b1fafd5ff931453f13c023c6c
LeeHS0805 <504794559@qq.com>
2022-08-21 23:40:58 +0800

    delete：dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as Button } from "./Components/Button";
-export { default as Menu } from "./Components/Menu";
-export { default as AutoComplete } from "./Components/AutoComplete";
-export { default as Icon } from "./Components/Icon";
-export { default as Input } from "./Components/Input";
-export { default as Progress } from "./Components/Progress";
-export { default as Transition } from "./Components/Transition";
-export { default as Upload } from "./Components/Upload";
